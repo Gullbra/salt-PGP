3f4ed954040d1901439001a101cfec3ef88f4b46
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-04 00:42:26 +0100

    started working on lab 2 in ASP webapi

---
+++ w1a2 addons-ts-lab-puppiesApi-main/Csharp/Controllers/PuppiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Csharp.Models;
+
+namespace Csharp.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class PuppiesController : ControllerBase
+{
+	private Db PuppyDb;
+
+	public PuppiesController()
+	{
+		_db = new Db();
+	}
+
+	[HttpGet]
+	public List<Puppy> GetAll()
+	{
+		return PuppyDb.GetList();
+	}
+}
---
+++ w1a2 addons-ts-lab-puppiesApi-main/Csharp/Models/Db.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp.Models;
+
+
+public class Db
+{
+	private List<Puppy> _db;
+
+	public Db()
+	{
+		_db = new List<Puppy>()
+		{
+			new Puppy() { Id = 1, Name = "Dogmeat", Breed = "German Sheepherd", BirthDate="2016-10-22"}
+		};
+	}
+
+	public List<Puppy> GetList()
+	{
+		return _db;
+	}
+}
