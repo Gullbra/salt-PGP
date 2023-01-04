f75e93b52598335fbe7cad36950bd924bac3a7eb
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-04 10:23:04 +0100

    added port from process argv

--- w1a2 addons-ts-lab-puppiesApi-main/TS/src/server/server.ts
+++ w1a2 addons-ts-lab-puppiesApi-main/TS/src/server/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
-const port = 3000;
+const port = process.argv[2] || 3000;
 
 app.listen(port, (): void => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
