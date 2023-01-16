c06acb6066afc0282d0a3dce0ddbeaf680a6734e
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-16 13:07:44 +0100

    working mocking of backend

--- week_3 tiny-blog/backend/src/app.ts
+++ week_3 tiny-blog/backend/src/app.ts
@@ -1,19 +1,28 @@
 
-import express from "express";
+import express, { response } from "express";
 import { Request, Response, Application } from 'express';
+import axios from 'axios'
+import fs from 'fs'
+import path from "path";
 
 const app: Application = express()
-
-// if (process.argv[2] && !isNaN(process.argv[2])) throw new Error('Invalid port')
-// const port = process.argv[2] || 3000
 const port:number = 3000
-const fetchUrl:string = process.env.development
-  ? '../mock/mock.data.ts'
-  : 'https://dummyjson.com/posts'
 
+const fetch = () => {
+  return process.env.development
+    ? fs.promises
+        .readFile(path.join(__dirname, '..', 'mock', 'mock.data.json'))
+        .then(data => JSON.parse(data.toString()))
+    : axios.get('https://dummyjson.com/posts')
+}
 
 app.use(express.json())
 
 app.route('/api/posts')
+  .get((req:Request, res:Response) => {
+    fetch()
+      .then(response => res.json(response.data))
+      .catch(err => res.send(err.message))
+  })
 
 app.listen(port, () => console.log(`📡 listening to http://localhost:${port}`))
