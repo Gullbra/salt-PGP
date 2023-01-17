32ba7dedf0dc7735491f448f01b2968ef2c91f14
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-17 16:57:46 +0100

    simple collapsable sidebar

--- week_3 tiny-blog/frontend/src/components/Layout.tsx
+++ week_3 tiny-blog/frontend/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/styling-Layout.css'
 import { HashLink } from 'react-router-hash-link';
 import { capitalize } from "../util/capitalize";
@@ -6,29 +6,40 @@ import { capitalize } from "../util/capitalize";
 const Layout = (
   {children, setOfTags}: {children: React.ReactNode, setOfTags: Set<string>}
 ) => {
+
+  const [ showSidebar, setShowSidebar ] = useState<boolean>(true)
+
   return (
     <>
       <header className="site__header">
+        {!showSidebar && (
+          <p onClick={() => setShowSidebar(true)}>☰</p>
+        )}
         <h1>This is a Header</h1>
       </header>
 
       <flex-wrapper class="site__wrapper">
-        <aside className="site__sidebar">
-          <nav>this is a nav bar</nav>
-          {setOfTags && Array.from(setOfTags).sort().map((tag, index) => (
-            <HashLink
-              smooth
-              className="sidebar__nav-item"
-              //smooth 
-              to={`/#${tag}Section`} 
-              key={index}
-            >{capitalize(tag)}
-            </HashLink>
-          ))}
-        </aside>
+
+        {showSidebar && (
+          <aside className="site__sidebar">
+            <nav onClick={() => setShowSidebar(false)}>this is a nav bar</nav>
+            {setOfTags && Array.from(setOfTags).sort().map((tag, index) => (
+              <HashLink
+                smooth
+                className="sidebar__nav-item"
+                //smooth 
+                to={`/#${tag}Section`} 
+                key={index}
+              >{capitalize(tag)}
+              </HashLink>
+            ))}
+          </aside>
+        )}
+
         <main className="site__main">
           {children}
         </main>
+
       </flex-wrapper>
     </>
   )
