0f57a5959b5e8a47796b09ed4afbe061e8c1174c
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-24 22:21:18 +0100

    feat: primitive GUI for pagination

--- week_4 code_test/frontend/src/components/ProductList.tsx
+++ week_4 code_test/frontend/src/components/ProductList.tsx
@@ -12,10 +12,17 @@ interface IListProps {
 }
 
 const ProductList = ({products, pagination, setPagination}:IListProps) => {
+  
+  // const paginationArr = Array.from({length: pagination.maxPages || 1}, (_, i) => i++)
+
+  const paginationHandler = (page:number) => {
+    setPagination ((prev) => {return {...prev, page: page}})
+  } 
 
   return (
     <flex-wrapper class="--flex-center">
-      <section className="test-class">Test</section>
+      <section className="test-class-1">Test</section>
+
       <list-wrapper class="main__list-wrapper">
         {products.map((product, index) => (
           <Link key={index} to={`/${product.id}`}
@@ -34,6 +41,21 @@ const ProductList = ({products, pagination, setPagination}:IListProps) => {
           </Link>
         ))}
       </list-wrapper>
+
+      {pagination.maxPages && (
+        <section className="test-class-1">
+          {pagination.page !== 1 && <button type="button" onClick={() => paginationHandler(1)}>{"<<"}</button>}
+          {/* <button type="button" onClick={() => paginationHandler(pagination.page-1)}>{"<"}</button> */}
+          {pagination.page-2 > 0 && (<span>...</span>)}
+          {pagination.page-1 > 0 && (<button type="button" onClick={() => paginationHandler(pagination.page-1)}>{`${pagination.page-1}`}</button>)}
+          <button type="button"><b>{`${pagination.page}`}</b></button>
+          {pagination.page+1 <= pagination.maxPages && (<button type="button" onClick={() => paginationHandler(pagination.page+1)}>{`${pagination.page+1}`}</button>)}
+          {pagination.page+2 <= pagination.maxPages && (<span>...</span>)}
+          {/* <button type="button">{">"}</button> */}
+          {pagination.page !== pagination.maxPages && <button type="button" onClick={() => paginationHandler(pagination.maxPages || 1)}>{">>"}</button>}
+        </section>
+      )}
+      
     </flex-wrapper> 
   )
 }
