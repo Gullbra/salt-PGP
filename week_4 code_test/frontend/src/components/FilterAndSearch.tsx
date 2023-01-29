42379e41d41b6605d1eb400249aa3bfb7d87e6cd
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-29 21:12:05 +0100

    end of sundays work

--- week_4 code_test/frontend/src/components/FilterAndSearch.tsx
+++ week_4 code_test/frontend/src/components/FilterAndSearch.tsx
@@ -28,7 +28,7 @@ const FilterAndSearch = ({ pageState, productState, pageStateHandler }: IPaginat
         }
       });
       console.log(newFilters)
-      pageStateHandler({filters: [...pageState.filters , ...newFilters]})  
+      pageStateHandler({filters: pageState.filters?.concat(newFilters) || newFilters})  
     }
   }
 
@@ -45,58 +45,73 @@ const FilterAndSearch = ({ pageState, productState, pageStateHandler }: IPaginat
 
   return (
     <>
-      <div className="box">
-        <input type="text" ref={searchInput} placeholder=" Search"/>
-        <button type="button" onClick={handleSearch}>search</button>
-        {pageState.search !== null 
-          ? <span onClick={() => {
-              console.log("testing remove search")
-              pageStateHandler({search: null})
-            }} className="active-filters-card">
-              <span>{decodeURIComponent(pageState.search)}</span>
-            </span> 
-          : ""
-        }
-      </div>
+      <flex-wrapper>
+        {/* //TODO: Add flex */}
+        <div className="box --search">
+          <input type="text" ref={searchInput} placeholder=" Search"/>
+          <button type="button" onClick={handleSearch}>search</button>
+          {pageState.search !== null 
+            ? <span onClick={() => {
+                console.log("testing remove search")
+                pageStateHandler({search: null})
+              }} className="active-filters-card">
+                <span>{decodeURIComponent(pageState.search)}</span>
+              </span> 
+            : ""
+          }
+        </div>
 
-      <div className="box section-search-filter__filter-dropdown">
-        <label className="filter-dropdown__dropdown-label">
-          <p className="label__text">Filter</p>
+        <div className="box --filter section-search-filter__filter-dropdown dropdown-container">
+          <label className="filter-dropdown__dropdown-label">
+            <p className="label__text">Filter</p>
 
-          {pageState.filters?.map((filter, index) => (
-            <span 
-              onClick={() => pageStateHandler({filters: pageState.filters.filter(item => item !==filter)})} 
-              key={index} className="active-filters-card">
-              <span>{decodeURIComponent(filter)}</span>
-            </span>
-          ))}
-        </label>
+            {pageState.filters?.map((filter, index) => (
+              <span 
+                onClick={() => pageStateHandler({filters: pageState.filters.filter(item => item !==filter)})} 
+                key={index} className="active-filters-card">
+                <span>{decodeURIComponent(filter)}</span>
+              </span>
+            ))}
+          </label>
 
-        <menu className="dropdown-menu">
-          <button className="filter-btn" onClick={handleFilterChange}>Apply new filters</button>
-          <ul ref={filterSelect}>
-            {productState.types
-              ?.filter(type => !pageState.filters?.includes(encodeURIComponent(type)))
-              .map(filter => { return (
-                  <li
-                    key={filter} 
-                    className="dropdown-menu__menu-item"
-                    value={filter}
-                    onClick={event => event.currentTarget.classList.contains("--menu-item-selected") 
-                      ? event.currentTarget.classList.remove("--menu-item-selected")
-                      : event.currentTarget.classList.add("--menu-item-selected")
-                    }
-                  >
-                    {`${filter}`}
-                  </li>
-              )})
-            }
-          </ul>
-        </menu>
-      </div> 
+          <menu className="dropdown-menu">
+            <button className="filter-btn" onClick={handleFilterChange}>Apply new filters</button>
+            <ul ref={filterSelect}>
+              {productState.types
+                ?.filter(type => !pageState.filters?.includes(encodeURIComponent(type)))
+                .map(filter => { return (
+                    <li
+                      key={filter} 
+                      className="dropdown-menu__menu-item"
+                      value={filter}
+                      onClick={event => event.currentTarget.classList.contains("--menu-item-selected") 
+                        ? event.currentTarget.classList.remove("--menu-item-selected")
+                        : event.currentTarget.classList.add("--menu-item-selected")
+                        // TODO: add debounce to this onclick
+                      }
+                    >{`${filter}`}</li>
+                )})
+              }
+            </ul>
+          </menu>
+        </div> 
+      </flex-wrapper>
 
-      <div className="box">
+      <flex-wrapper>
+        {/* //TODO: map filter cards here*/}
+      </flex-wrapper>
+
+      <div className="box --prod-count"
+        // TODO: set pagelimit
+        // className="box dropdown-container"
+      >
         <p>{`Showing ${productState.results.length} of ${productState.count} products`}</p>
+        {/* <menu className="dropdown-menu">
+          <div>
+            <label htmlFor="setPageLimit">set pagelimit: </label>
+            <input type="number" id="setPageLimit" min={1} max={productState.count} defaultValue={pageState.limit}/>
+          </div>
+        </menu> */}
       </div>
     </>
   )
