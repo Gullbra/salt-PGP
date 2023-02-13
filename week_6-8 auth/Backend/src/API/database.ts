498d9b718342d899527c27c785883431515d4d5f
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-02-13 15:29:32 +0100

    database reading mock data async in ts

--- week_6-8 auth/Backend/src/API/database.ts
+++ week_6-8 auth/Backend/src/API/database.ts
@@ -7,79 +7,102 @@ import { IProductsSQL, IStoreSQL, IUserSQL } from '../interfaces/interfaces';
 
 const DBSOURCE = path.join("@root", "database", "db.sqlite")
 
-const GetUsersAsJson = () => JSON.parse(fs.readFileSync("@root/database/mock/User_Mock_data.json").toString()) as IUserSQL[];
-const GetStoresAsJson = () => JSON.parse(fs.readFileSync("@root/database/mock/Store_Mock_data.json").toString()) as IStoreSQL[];
-const GetProductsAsJson = () => JSON.parse(fs.readFileSync("@root/database/mock/Products_Mock_data.json").toString()) as IProductsSQL[];
+const getUsersAsJson = (): Promise<IUserSQL[]> => fs.promises
+  .readFile("@root/database/mock/User_Mock_data.json")
+  .then(data => JSON.parse(data.toString()));
+
+const getStoresAsJson = (): Promise<IStoreSQL[]> => fs.promises
+  .readFile("@root/database/mock/Store_Mock_data.json")
+  .then(data => JSON.parse(data.toString()));
+
+const getProductsAsJson = (): Promise<IProductsSQL[]> => fs.promises
+  .readFile("@root/database/mock/Products_Mock_data.json")
+  .then(data => JSON.parse(data.toString()))
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-  if (err) throw err
-  console.log(`\nConnected to SQLite database at \"${DBSOURCE}\"`);
+  if (err) throw err; console.log(`\nConnected to SQLite database at \"${DBSOURCE}\"`);
 
-  const users = GetUsersAsJson();
   db.run(
-    //TODO:rew
-    `CREATE TABLE UserData (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email VARCHAR(50),
-    password VARCHAR(50),
-    role VARCHAR(11),
-    storeId INT
-    )`,
+    `CREATE TABLE user_data (
+      user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      email VARCHAR(50),
+      password VARCHAR(50),
+      role VARCHAR(11),
+      store_id INT
+    );`, 
     err => {
-      if (err) return console.log('Already User-table there');
+      if (err) return console.log ('Already User-table there');
 
-      // Table just created, creating some rows
-      const insert = 'INSERT INTO UserData (id, email, password, role, storeId) VALUES (?,?,?,?,?)';
-      users.map((newUser) => db.run(
-        insert, 
-        [newUser.id, newUser.email, newUser.password, newUser.role, newUser.uniqueStoreId]
-      ))
-      console.log(`${users.length} Users created`);
+      getUsersAsJson().then(users => {
+        users.map((newUser) => db.run(
+          `INSERT INTO user_data (user_id, email, password, role, store_id) 
+          VALUES (?,?,?,?,?)`, 
+          [newUser.user_id, newUser.email, newUser.password, newUser.role, newUser.store_id]
+        ))
+        console.log(`${users.length} User(s) created`);
+      })
     }
   )
-
-  const stores = GetStoresAsJson();
   db.run(
-    `CREATE TABLE StoreData (
-    name VARCHAR(50),
-    uniqueStoreId INT
-    )`,
+    `CREATE TABLE store_data (
+      store_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(50)
+    );`, 
     err => {
       if (err) return console.log('Already Store-table there');
-          
-      // Table just created, creating some rows
-      const insert = 'INSERT INTO StoreData (name, uniqueStoreId) VALUES (?,?)';
-      stores.map(newStore => db.run(
-        insert, 
-        [newStore.name, newStore.uniqueStoreId]
-      ))
-      console.log(`${stores.length} Stores created`);
-    }
-  );
 
-  const products = GetProductsAsJson();
+      getStoresAsJson().then(stores => {
+        stores.map((newStore) => db.run(
+          `INSERT INTO store_data (store_id, name) 
+          VALUES (?,?)`, 
+          [newStore.store_id, newStore.name]
+        ))
+        console.log(`${stores.length} Store(s) created`);
+      })
+    }
+  )
   db.run(
-    `CREATE TABLE ProductData (
-    id INT, 
-    title VARCHAR(50), 
-    description TEXT, 
-    imageUrl VARCHAR(50),
-    storeId INT, 
-    price VARCHAR(50), 
-    quantity INT, 
-    category VARCHAR(50));`,
+    `CREATE TABLE product_data (
+      product_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      title VARCHAR(50), 
+      description TEXT, 
+      image_url VARCHAR(50),
+      store_id INT, 
+      price VARCHAR(50), 
+      quantity INT, 
+      category VARCHAR(50)
+    );`, 
     err => {
-      if (err) return console.log('Already Products-table there');
+      if (err) return console.log('Already Product-table there');
 
-      // Table just created, creating some rows
-      const insert = 'INSERT INTO ProductData (id, title, description, imageUrl, storeId, price, quantity, category) VALUES (?,?,?,?,?,?,?,?)';
-      products.map(product => db.run(
-        insert, 
-        [product.id, product.title, product.description, product.imageUrl, product.storeId, product.price, product.quantity, product.category]
-      ))
-      console.log(`${products.length} Products created`);
+      getProductsAsJson().then(products => {
+        products.map((newProduct) => db.run(
+          `INSERT INTO product_data (
+            product_id, 
+            title, 
+            description, 
+            image_url, 
+            store_id, 
+            price, 
+            quantity, 
+            category
+          ) 
+          VALUES (?,?,?,?,?,?,?,?)`, 
+          [
+            newProduct.product_id, 
+            newProduct.title, 
+            newProduct.description, 
+            newProduct.image_url, 
+            newProduct.store_id, 
+            newProduct.price, 
+            newProduct.quantity, 
+            newProduct.category
+          ]
+        ))
+        console.log(`${products.length} Products(s) created`);
+      })
     }
-  );
+  )
 });
 
 // ** Close connection:
