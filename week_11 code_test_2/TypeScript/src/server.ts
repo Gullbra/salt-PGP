d7ac55cf05c7775d2ded0dd13fe0b1d085416673
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-03-07 17:13:36 +0100

    week 11: TS: first required task

--- week_11 code_test_2/TypeScript/src/server.ts
+++ week_11 code_test_2/TypeScript/src/server.ts
@@ -8,7 +8,47 @@ const envObj = {
   port: 3005
 }
 
-app.route('*').get((_, res) => fetcheroo().then(data => res.json(data)))
+interface IItinerary {
+  flight_id: string,
+  departureAt: string,
+  arrivalAt: string,
+  availableSeats: number,
+  prices: {
+    currency: string,
+    adult: number,
+    child: number
+  }
+}
+
+interface IRoutes {
+  route_id: string,
+  departureDestination: string,
+  arrivalDestination: string,
+  itineraries: IItinerary[]
+}
+
+app.route('/api/').get((_, res) => fetcheroo().then(data => res.json(data)))
+app.route('/api/routes/').get(async (req, res) => {
+  fetcheroo()
+    .then((routes:IRoutes[]) => {
+      if(!req.query.routeId && (!req.query.departure || !req.query.destination)) 
+        return routes
+
+      if(req.query.routeId)
+        return routes.filter(route => route.route_id === String(req.query.routeId))
+
+      return routes.filter(route => {
+        return (
+          route.departureDestination === String(req.query.departure) && 
+          route.arrivalDestination === String(req.query.destination)
+        )
+      })
+    })
+    .then(filteredRoutes => res.json(filteredRoutes))
+    .catch(err => res.json(`Should not be possible, what have you done? \n\n${err.message}`))
+})
+
+app.route('*').get((_, res) => res.send(`<h1>404 - Nothing here</h1>\n<p>Try <a href=\"http://localhost:${envObj.port}/api/\">/api/</a>instead<p>`))
 
 
 app.listen(envObj.port, () => console.log(`📡 server listening to http://localhost:${envObj.port}`))
