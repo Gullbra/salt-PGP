ad5f9bf7fe9482e06a835b6983db1c8768793e6b
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-03-07 22:35:20 +0100

    week 11: TS: second required task

--- week_11 code_test_2/TypeScript/src/server.ts
+++ week_11 code_test_2/TypeScript/src/server.ts
@@ -20,32 +20,64 @@ interface IItinerary {
   }
 }
 
-interface IRoutes {
+interface IRoute {
   route_id: string,
   departureDestination: string,
   arrivalDestination: string,
   itineraries: IItinerary[]
 }
 
-app.route('/api/').get((_, res) => fetcheroo().then(data => res.json(data)))
-app.route('/api/routes/').get(async (req, res) => {
+app.route('/api').get((_, res) => fetcheroo().then(data => res.json(data)))
+
+// ! Accepts query params: routeId, departure, arrival
+app.route('/api/routes').get(async (req, res) => {
   fetcheroo()
-    .then((routes:IRoutes[]) => {
-      if(!req.query.routeId && (!req.query.departure || !req.query.destination)) 
+    .then((routes: IRoute[]) => {
+      if(!req.query.routeId && !req.query.departure && !req.query.arrival) 
         return routes
 
       if(req.query.routeId)
         return routes.filter(route => route.route_id === String(req.query.routeId))
 
-      return routes.filter(route => {
-        return (
-          route.departureDestination === String(req.query.departure) && 
-          route.arrivalDestination === String(req.query.destination)
-        )
-      })
+      if (!req.query.departure)
+        return routes.filter(route => route.arrivalDestination === String(req.query.arrival))
+
+      if (!req.query.arrival)
+        return routes.filter(route => route.departureDestination === String(req.query.departure))
+
+      return routes.filter(route => (
+        route.departureDestination === String(req.query.departure) && 
+        route.arrivalDestination === String(req.query.arrival)
+      ))
     })
     .then(filteredRoutes => res.json(filteredRoutes))
     .catch(err => res.json(`Should not be possible, what have you done? \n\n${err.message}`))
+})
+
+// ! Accepts query params: lowLimit, highLimit, departure, arrival
+app.route('/api/routes/:routeId/itineraries').get((req, res) => {
+  fetcheroo()
+    .then((routes: IRoute[]) => {
+      const route = routes.find(route => route.route_id === req.params.routeId)
+      
+      if (!route) return res.json("Invalid routeId: No route found")
+
+      if (!req.query.lowLimit || !req.query.highLimit || (!req.query.departure && !req.query.arrival)) {
+        return route.itineraries
+      }
+
+      return route.itineraries.filter(itinerary => {
+        const timeItinerary = req.query.departure 
+          ? new Date(itinerary.departureAt).getTime()
+          : new Date(itinerary.arrivalAt).getTime()
+
+        const timeLowLimit = new Date(String(req.query.lowLimit)).getTime()
+        const timeHighLimit = new Date(String(req.query.highLimit)).getTime()
+
+        return timeLowLimit <= timeItinerary && timeHighLimit >= timeItinerary
+      })
+    })
+    .then(filteredItineraries => res.json(filteredItineraries))
 })
 
 app.route('*').get((_, res) => res.send(`<h1>404 - Nothing here</h1>\n<p>Try <a href=\"http://localhost:${envObj.port}/api/\">/api/</a>instead<p>`))
